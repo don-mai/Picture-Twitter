b3f3310d102221f83f48e120b53e3fd996de342b
don_mai <nobunon76@gmail.com>
2021-05-01 19:24:52 +0900

    get-tweet.pyの編集

--- get-tweet.py
+++ get-tweet.py
@@ -5,6 +5,18 @@ import twitter
 
 #apikeyの取得
 
+
+
+
+ck=
+cs=
+at=
+ats=
+
+api = twitter.Api(ck,cs,at,ats)
 #タイムラインの取得
+user = 955610533553557504
+statues = api.GetUserTikeline(user)
 
 #出力
+print([s.text for s in statuses], '\n')
